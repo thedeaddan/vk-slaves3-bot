95643daa3501db7a602258d6221bc28d8edbf5c5
Даниил <40400854+thedeaddan@users.noreply.github.com>
2021-04-11 00:10:18 +0300

    актуалочка

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# github.com/monosans/vk-slaves3-bot
 authorization = ""
-my_id = Ur ID
+my_id = 607137534
 buy_slaves_mode = 1
 set_fetters = 1
-buy_from_ids = [411634368,12334234]
+check_was_in_app = 1
+sell_low_profit_slaves = 1
+buy_from_ids = [411634368, 12334234]
 min_price = 20
 max_price = 500
 min_delay = 3.34
 max_delay = 3.5
-sell_low_slaves = 1
-check_offline = 0
